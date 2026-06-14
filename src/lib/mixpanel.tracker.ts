1cca539430695554fc2f4433e670c1e5caa5b530
Paras ohri <ohriparas2005@gmail.com>
2026-06-14 20:27:23 +0530

    added mixpanel

--- src/lib/mixpanel.tracker.ts
+++ src/lib/mixpanel.tracker.ts
@@ -1,6 +1,6 @@
 import mixpanel from "mixpanel-browser";
 
-mixpanel.init('606c3e4ba62824543225eb00a2ebd3c7', {
+mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_ID!, {
   api_host: "/api/mp",
   autocapture: true,
   record_sessions_percent: 100,
@@ -9,8 +9,6 @@ mixpanel.init('606c3e4ba62824543225eb00a2ebd3c7', {
 export default mixpanel;
 
 export function trackGuestContinue(redirectTo: string) {
-  console.log("dsa");
-  
   mixpanel.track("Guest Continue Clicked", {
     redirect_to: redirectTo,
     source: "sign_in_page",
